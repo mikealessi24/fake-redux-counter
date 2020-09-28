bd97ccd6ec52b5ce2085797141312158346f8e4a
mikealessi24 <60523720+mikealessi24@users.noreply.github.com>
2020-09-28 19:18:51 -0400

    increment and decrement by n

--- src/App.js
+++ src/App.js
@@ -2,25 +2,37 @@ import React, { useReducer } from "react";
 
 const INCREMENT_COUNTER = "INCREMENT_COUNTER";
 const DECREMENT_COUNTER = "DECREMENT_COUNTER";
+const UPDATE_N = "UPDATE_N";
 
-function incrementCounter() {
+function incrementCounter(n) {
   return {
     type: INCREMENT_COUNTER,
+    n: n,
   };
 }
 
-function decrementCounter() {
+function decrementCounter(n) {
   return {
     type: DECREMENT_COUNTER,
+    n: n,
+  };
+}
+
+function updateN(n) {
+  return {
+    type: UPDATE_N,
+    n: n,
   };
 }
 
 const reducer = (state, action) => {
   switch (action.type) {
     case INCREMENT_COUNTER:
-      return { ...state, count: state.count + 1 };
+      return { ...state, count: state.count + state.n };
     case DECREMENT_COUNTER:
-      return { ...state, count: state.count - 1 };
+      return { ...state, count: state.count - state.n };
+    case UPDATE_N:
+      return { ...state, n: action.n };
     default:
       return state;
   }
@@ -28,13 +40,20 @@ const reducer = (state, action) => {
 
 const initialState = {
   count: 0,
+  n: 1,
 };
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state);
   return (
     <div>
       <div>{state.count}</div>
+      <label>increment by n: </label>
+      <input
+        onChange={(event) => dispatch(updateN(Number(event.target.value)))}
+        placeholder="Default n = 1"
+      />
       <button onClick={() => dispatch(incrementCounter())}>Increment</button>
       <button onClick={() => dispatch(decrementCounter())}>Decrement</button>
     </div>
