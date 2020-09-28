3fabcaf1387075097cff6b5f41fb118b22294935
mikealessi24 <60523720+mikealessi24@users.noreply.github.com>
2020-09-28 18:42:48 -0400

    changed useState to useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const reducer = (state, action) => {
+  if (action.type === "INCREMENT_COUNTER") {
+    return { ...state, count: state.count + 1 };
+  } else if (action.type === "DECREMENT_COUNTER") {
+    return { ...state, count: state.count - 1 };
+  }
+  return state;
+};
+
+const initialState = {
+  count: 0,
+};
 
 function App() {
-  const [count, setCount] = React.useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div>
-      <div>{count}</div>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <div>{state.count}</div>
+      <button onClick={() => dispatch({ type: "INCREMENT_COUNTER" })}>
+        Increment
+      </button>
+      <button onClick={() => dispatch({ type: "DECREMENT_COUNTER" })}>
+        Decrement
+      </button>
     </div>
   );
 }
